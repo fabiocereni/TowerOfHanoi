c9ea444df2f4e04a52b64be897c2336486f76484
fabio.cereni <fabio.cereni@supsi.ch>
2025-11-12 12:20:03 +0100

    update gitignore

--- engine/doc/html/files_dup.js
+++
@@ -1,5 +0,0 @@
-var files_dup =
-[
-    [ "engine.cpp", "engine_8cpp.html", "engine_8cpp" ],
-    [ "engine.h", "engine_8h.html", "engine_8h" ]
-];
--- engine/doc/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['engine_2ecpp_0',['engine.cpp',['../engine_8cpp.html',1,'']]],
-  ['engine_2eh_1',['engine.h',['../engine_8h.html',1,'']]]
-];
