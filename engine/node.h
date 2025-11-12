74688ab1bb67bb1a8cb52f34a5c0db34c35ee8c6
simone.torri <simone.torri@supsi.ch>
2025-11-12 11:30:03 +0100

    shared pointers update

--- engine/node.h
+++ engine/node.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "engine.h"    
+#include "engine.h"
 #include "object.h"
 #include <glm/mat4x4.hpp>
 #include <vector>
@@ -11,8 +11,8 @@ namespace Eng {
    class ENG_API Node : public Object {
    protected:
       glm::mat4 matrix;
-      std::unique_ptr<Node> parent;
-      std::vector<std::unique_ptr<Node>> children;
+      std::shared_ptr<Node> parent;
+      std::vector<std::shared_ptr<Node>> children;
 
    public:
       Node();
@@ -22,19 +22,19 @@ namespace Eng {
       Node& operator=(const Node& other);
       Node& operator=(Node&& other) noexcept;
 
-      // pure virtual render (Node è astratta)
+      // pure virtual render (Node ï¿½ astratta)
       virtual void render() = 0;
 
       glm::mat4 getMatrix() const;
       void setMatrix(const glm::mat4& matrix);
 
-      std::unique_ptr<Node>& getParent();
-      void setParent(std::unique_ptr<Node> parent);
+      std::shared_ptr<Node>& getParent();
+      void setParent(std::shared_ptr<Node> parent);
 
-      std::vector<std::unique_ptr<Node>>& getChildren();
-      void addChildren(std::unique_ptr<Node> child);
-      std::unique_ptr<Node> removeChildren(const std::string& name);
-      std::unique_ptr<Node> returnChild(const std::string& name);
+      std::vector<std::shared_ptr<Node>>& getChildren();
+      void addChildren(std::shared_ptr<Node> child);
+      std::shared_ptr<Node> removeChildren(const std::string& name);
+      std::shared_ptr<Node> returnChild(const std::string& name);
       glm::mat4 getWorldMatrix() const;
    };
 
