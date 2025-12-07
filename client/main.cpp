679a3f03d1b47bb215dfcfc914231f26fd69c8aa
simone.torri <simone.torri@student.supsi.ch>
2025-12-08 00:23:32 +0100

    dynamic camera

--- client/main.cpp
+++ client/main.cpp
@@ -6,19 +6,22 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <iostream>
 #include <filesystem>
 
 namespace fs = std::filesystem;
+
 
 fs::path sourceFile = __FILE__;
 fs::path projectDir = sourceFile.parent_path().parent_path();
 fs::path modelPath = projectDir / "ExportProgetto" / "ProvaTavoloovoNew.ovo";
 
-
+auto dynamic_cam_pos = glm::vec3(0.0f, 800.0f, 1600.0f);
+float dynamic_cam_x_angle = 0.0f;
+float dynamic_cam_y_angle = 0.0f;
+float dynamic_cam_y = 800.0f;
 
 std::shared_ptr<Eng::Camera> returnFrontTableCamera(Eng::Base& eng) {
-    const auto frontTableCam = eng.createPerspectiveCamera(40, 800.f / 600.f, 0.1f, 20000.0f);
+    const auto frontTableCam = eng.createPerspectiveCamera(40, 1800.f / 1000.f, 0.1f, 20000.0f);
 
 
     constexpr auto frontTablePosition = glm::vec3(0.0f, 1600.0f, 400.0f);
@@ -32,17 +35,34 @@ std::shared_ptr<Eng::Camera> returnFrontTableCamera(Eng::Base& eng) {
 }
 
 std::shared_ptr<Eng::Camera> returnTopTableCamera(Eng::Base& eng) {
-    const auto cam2 = eng.createPerspectiveCamera(40, 800.f / 600.f, 0.1f, 20000.0f);
+    const auto topTableCam = eng.createPerspectiveCamera(40, 1800.f / 1000.f, 0.1f, 20000.0f);
 
     // posizione sopra al tavolo
-    constexpr auto cam2Pos = glm::vec3(0.0f, 3000.0f, -700.0f);
+    constexpr auto topTableCamPosition = glm::vec3(0.0f, 3000.0f, -700.0f);
 
     const glm::mat4 cam2WorldMatrix =
-        glm::translate(glm::mat4(1.0f), cam2Pos) *
+        glm::translate(glm::mat4(1.0f), topTableCamPosition) *
         glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1, 0, 0));
 
-    cam2->setMatrix(cam2WorldMatrix);
-    return cam2;
+    topTableCam->setMatrix(cam2WorldMatrix);
+    return topTableCam;
+}
+
+void updateDynamicCamera(const std::shared_ptr<Eng::Camera>& cam) {
+
+    dynamic_cam_pos.y = dynamic_cam_y;
+
+    const glm::mat4 x_rot = glm::rotate(glm::mat4(1.0f),
+                    glm::radians(dynamic_cam_x_angle),
+                        glm::vec3(0, 1, 0));
+
+    const glm::mat4 y_rot = glm::rotate(glm::mat4(1.0f),
+                    glm::radians(dynamic_cam_y_angle),
+                        glm::vec3(1, 0, 0));
+
+    const glm::mat4 trans = glm::translate(glm::mat4(1.0f), dynamic_cam_pos);
+
+    cam->setMatrix(trans * x_rot * y_rot);
 }
 
 std::vector<std::shared_ptr<Eng::Texture>> loadAndReturnTextures(const Eng::Base& eng) {
@@ -70,26 +90,27 @@ void createAndReturnOmniDirectionalLight(Eng::Base& eng, const std::shared_ptr<E
     my_omnilight->setAmbient(glm::vec3(1.0f));
     my_omnilight->setDiffuse(glm::vec3(0.5f));
     my_omnilight->setSpecular(glm::vec3(0.5f));
-
+    my_omnilight->setName("OmnidirectionalLight");
 
     sceneRoot->addChildren(my_omnilight);
 }
 
+
+
 int main(const int argc, char** argv) {
-    // 1. Esegui i test
 
 #ifdef DEBUG
     TestSuite::runAllTests();
 #endif
 
-    // 2. Inizializza l'Engine
+    // Inizializza l'Engine
     Eng::Base& eng = Eng::Base::getInstance();
     eng.init(argc, argv, "Hanoi Tower");
 
 
-    // 3. Caricamento della Scena
+    // Caricamento della Scena
     const std::string scenePath = modelPath.string();
-    std::shared_ptr<Eng::Node> sceneRoot = eng.load(scenePath);
+    const auto sceneRoot = eng.load(scenePath);
 
 
     const std::vector<std::shared_ptr<Eng::Texture>> textures = loadAndReturnTextures(eng);
@@ -100,13 +121,11 @@ int main(const int argc, char** argv) {
     HanoiGame game(sceneRoot);
 
 
-
-    // ---------------------------------
-
     eng.setActiveCamera(returnFrontTableCamera(eng));
 
     const auto renderList = std::make_shared<Eng::List>();
 
+    auto cam3 = eng.createPerspectiveCamera(40, 1800.f / 1000.f, 0.1f, 20000.0f);
 
     eng.overrideKeyboardCallback([&](const unsigned char key, const int mouseX, const int mouseY){
         switch (key)
@@ -120,6 +139,33 @@ int main(const int argc, char** argv) {
         case '3':
             game.processInput(3);
             break;
+        case 'w':
+            dynamic_cam_pos.z -= 15.0f;
+            updateDynamicCamera(cam3);
+            break;
+        case 's':
+            dynamic_cam_pos.z += 15.0f;
+            updateDynamicCamera(cam3);
+            break;
+        case 'a':
+            dynamic_cam_x_angle += 0.5f;
+            updateDynamicCamera(cam3);
+            break;
+        case 'd':
+            dynamic_cam_x_angle -= 0.5f;
+            updateDynamicCamera(cam3);
+            break;
+
+        case 'u':
+            dynamic_cam_y_angle += 0.5f;
+            updateDynamicCamera(cam3);
+            break;
+
+        case 'g':
+            dynamic_cam_y_angle -= 0.5f;
+            updateDynamicCamera(cam3);
+            break;
+
         default:
             // Ignora altri tasti
             break;
@@ -127,16 +173,31 @@ int main(const int argc, char** argv) {
     });
 
     auto cam1 = returnFrontTableCamera(eng);
-    eng.overrideF1KeyBehaviour([&eng, &cam1]() {eng.setActiveCamera(cam1);});
+    eng.overrideF1KeyBehaviour([&eng, &cam1] {eng.setActiveCamera(cam1);});
 
     auto cam2 = returnTopTableCamera(eng);
-    eng.overrideF2KeyBehaviour([&cam2, &eng](){eng.setActiveCamera(cam2);});
+    eng.overrideF2KeyBehaviour([&cam2, &eng] {eng.setActiveCamera(cam2);});
+
+    updateDynamicCamera(cam3);
+
+    eng.overrideF3KeyBehaviour([&cam3, &eng] {eng.setActiveCamera(cam3);});
+
+    eng.overrideUpArrowBehaviour([&] {
+        dynamic_cam_y += 15.0f;
+        updateDynamicCamera(cam3);
+    });
+
+
+    eng.overrideDownArrowBehaviour([&]{
+        dynamic_cam_y -= 15.0f;
+        updateDynamicCamera(cam3);
+    });
+
 
     // 5. Ciclo di Rendering
     while (eng.update()) {
         eng.clear();
         renderList->clear();
-
 
         // --- DEBUG SETTINGS ---
         // Attiva Wireframe per vedere la struttura dell'oggetto
@@ -159,11 +220,9 @@ int main(const int argc, char** argv) {
         eng.end3D();
 
         eng.showFps();
-        eng.infoPrinter(game.getStatusMessage());
+        Eng::Base::infoPrinter(game.getStatusMessage());
 
         eng.swap();
     }
-
-
     return 0;
 }
